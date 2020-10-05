206a0e5722915a262f1a71b7a2b2a145d6b019c8
noelsebu <noelsebastian721@gmail.com>
2020-10-05 13:51:21 +0530

    tour of heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatBadge, MatBadgeModule} from '@angular/material/badge';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatListModule} from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './server/home/home.component';
+import { HeroesComponent } from './server/heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroDetailComponent } from './server/hero-detail/hero-detail.component';
+import { MessagesComponent } from './server/messages/messages.component';
+import { DashboardComponent } from './server/dashboard/dashboard.component';
+import {HeroSearchComponent } from './server/hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
-  ],
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot( InMemoryDataService, { dataEncapsulation: false } ),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatBadgeModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatSelectModule,
+   
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/server/mock-data.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Spiderman' },
+  { id: 3, name: 'Ironman' },
+  { id: 4, name: 'Hulk' },
+  { id: 5, name: 'Flash' },
+  { id: 6, name: 'Hitman' },
+  { id: 7, name: 'Batman' },
+  { id: 8, name: 'Aquaman' },
+  { id: 9, name: 'Shaktiman' }
+];
